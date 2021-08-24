ced20e7e2de05988e48a2517d797ece776d67d9f
justinsidechow <justinsidechow@gmail.com>
2021-08-24 03:16:04 -0700

    name changes, sign_in started

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
---
+++ frontend/src/components/sign_in.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import "./sign_in";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const sign_in = () => {
+  return (
+    <main class="form-signin">
+      <form>
+        <img
+          class="mb-4"
+          src="../assets/brand/bootstrap-logo.svg"
+          alt=""
+          width="72"
+          height="57"
+        />
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+        <div class="form-floating">
+          <input
+            type="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+
+        <div class="checkbox mb-3">
+          <label>
+            <input type="checkbox" value="remember-me" /> Remember me{" "}
+          </label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">
+          Sign in
+        </button>
+        <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+      </form>
+    </main>
+  );
+};
+
+export default sign_in;
