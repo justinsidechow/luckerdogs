8efbad0af02b7a899eba39e4996f6090d2e94c4f
justinsidechow <justinsidechow@gmail.com>
2021-09-17 03:16:46 -0700

    fixed card grid in Apps

---
+++ frontend/src/components/home_grids.js
@@ -0,0 +1,11 @@
+const home_grids = [
+  ["Coin Toss", "Feeling lucky punk?", "/spinning_coin.mp4"],
+  ["Lottery", "This is how you lose money!", "/lottery.mp4"],
+];
+
+// https://coverr.co/videos/head-or-tails-0FW9yIS3na
+
+// https://www.pexels.com/video/a-close-up-shot-of-a-pile-of-money-5466776/
+// Video by olia danilevich from Pexels
+
+export default home_grids;
