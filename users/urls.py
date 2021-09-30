24f2009a1315cae5c594748db3bba0d7c316856b
justinsidechow <justinsidechow@gmail.com>
2021-09-30 03:04:45 -0700

    extracted from db_model, cleaner code

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from os import name
+from django.urls import path, include
+from rest_framework import routers
+from .views import UserView
+from .views import CustomUserCreate, BlacklistTokenUpdateView
+
+app_name = 'users'
+
+urlpatterns = [
+    path('create/', CustomUserCreate.as_view(), name="create_user"),
+    path('logout/blacklist/', BlacklistTokenUpdateView.as_view(),
+         name='blacklist')
+]
