77295d923f258d94b92ab068a8617c9c7a48fecd
ChowJustin <justinchow310@gmail.com>
2022-03-16 03:29:17 -0700

    now works with redux reducer and included SaaS MIT License

--- frontend/src/components/signup/SignupActions.js
+++ frontend/src/components/signup/SignupActions.js
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2020 SaaSitive
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 import axios from "axios";
 import { toast } from "react-toastify";
 import { isEmpty } from "../utils/Utils";
@@ -6,9 +30,9 @@ import {
   CREATE_USER_SUBMITTED,
   CREATE_USER_SUCCESS,
 } from "./SignupTypes";
+import { Navigate } from "react-router-dom";
 
 export const signupNewUser = (userData) => (dispatch) => {
-  console.log("signupNewUser: " + userData.username);
   dispatch({ type: CREATE_USER_SUBMITTED }); // set submitted state
   axios
     .post("/api/v1/users/", userData)
@@ -19,6 +43,7 @@ export const signupNewUser = (userData) => (dispatch) => {
           " created successfully. Please login."
       );
       dispatch({ type: CREATE_USER_SUCCESS });
+      <Navigate to="/login" />;
     })
     .catch((error) => {
       if (error.resposne) {
@@ -30,12 +55,70 @@ export const signupNewUser = (userData) => (dispatch) => {
           errorData: error.response.data,
         });
       } else if (error.message) {
-        // the error message is available,
-        // let's display it on error toast
-        toast.error(JSON.stringify(error.message));
+        const keys = Object.keys(error.response.data);
+        const values = Object.values(error.response.data);
+        const result = Object.assign(
+          ...keys.map((k, i) => ({ [k]: values[i] }))
+        );
+        for (let key in result) {
+          //Cleaning up the array message by removing ',' from API so it looks cleaner
+          let message = String(result[key]).replace(",", " ");
+          toast.error(key + " - " + message);
+        }
+        dispatch({
+          type: CREATE_USER_ERROR,
+          errorData: error.response.data,
+        });
       } else {
         // strange error, just show it
         toast.error(JSON.stringify(error));
+        dispatch({
+          type: CREATE_USER_ERROR,
+          errorData: error.response.data,
+        });
       }
     });
 };
+
+/*
+if (userData.password !== userData.confirm_password) {
+      toast.error("The password and confirm password do not match.");
+    } else {
+      axios
+        .post("/api/v1/users/", userData)
+        .then((response) => {
+          toast.success(
+            "Account for " +
+              userData.username +
+              " created successfully. Please login."
+          );
+          dispatch({ type: CREATE_USER_SUCCESS });
+          <Navigate to="/login" />;
+        })
+        .catch((error) => {
+          if (!error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            toast.error(JSON.stringify(error.message));
+          } else if (error.message) {
+            const keys = Object.keys(error.response.data);
+            const values = Object.values(error.response.data);
+            const result = Object.assign(
+              ...keys.map((k, i) => ({ [k]: values[i] }))
+            );
+            for (let key in result) {
+              //Cleaning up the array message by removing ',' from API so it looks cleaner
+              let message = String(result[key]).replace(",", " ");
+              toast.error(key + " - " + message);
+              dispatch({
+                type: CREATE_USER_ERROR,
+                errorData: error.response.data,
+              });
+            }
+          } else {
+            // all other error
+            toast.error(JSON.stringify(error));
+          }
+        });
+    }
+  */
---
+++ frontend/src/components/signup/register.js
@@ -0,0 +1,176 @@
+import React, { useState } from "react";
+import { useNavigate, Navigate } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { FormControl } from "@material-ui/core";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { signupNewUser } from "./SignupActions";
+
+const useStyles = makeStyles((theme) => ({
+  container: {},
+  paper: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+function Register(props) {
+  const initialState = Object.freeze({
+    email: "",
+    username: "",
+    password: "",
+    confirm_password: "",
+  });
+  const [state, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      // Trimming any whitespace
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
+  const handleSubmit = () => {
+    const userData = {
+      username: state.username,
+      password: state.password,
+      confirm_password: state.confirm_password,
+    };
+    props.signupNewUser(userData);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <Container className={classes.container} component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}></Avatar>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <FormControl className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            {/*
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                type="email"
+                id="email"
+                name="email"
+                label="Email Address"
+                autoComplete="email"
+                onChange={handleChange}
+              />
+            </Grid>
+            */}
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                type="text"
+                id="username"
+                name="username"
+                label="Username"
+                autoComplete="username"
+                //value={state.username}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                type="password"
+                id="password"
+                name="password"
+                label="Password"
+                autoComplete="current-password"
+                //value={state.password}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                type="password"
+                id="confirm_password"
+                name="confirm_password"
+                label="Confirm Password"
+                //value={state.confirm_password}
+                onChange={handleChange}
+              />
+            </Grid>
+            {/* 
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          */}
+          </Grid>
+          <Button
+            //type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign In
+              </Link>
+            </Grid>
+          </Grid>
+        </FormControl>
+      </div>
+    </Container>
+  );
+}
+
+Register.propTypes = {
+  signupNewUser: PropTypes.func.isRequired,
+  createUser: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  createUser: state.createUser,
+});
+
+export default connect(mapStateToProps, {
+  signupNewUser,
+})(Register);
