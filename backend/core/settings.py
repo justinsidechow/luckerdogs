230e3e686acdcd94e02dcb09e653d5349256ad3f
ChowJustin <justinchow310@gmail.com>
2022-05-10 01:47:48 -0700

    fixed email smtp to account activation and included .env for more security

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,21 +11,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-p = Path(__file__).with_name("secret_key.txt")
-with p.open("r") as f:
-    SECRET_KEY = f.read().strip()
+SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0',
                  'luckerdogs.com', 'www.luckerdogs.com']
@@ -163,9 +163,25 @@ DJOSER = {
     },
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# django.core.mail.backends.locmem.EmailBackend for production
-SITE_NAME = "luckerdogs"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+# Custom setting. To email
+RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
+
+# touch .env
+# EMAIL_HOST=smtp.gmail.com
+# EMAIL_HOST_USER=YourEmail@address
+# EMAIL_HOST_PASSWORD=YourAppPassword
+# RECIPIENT_ADDRESS=TheRecieverOfTheMails
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' for google cloud smtp
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' for test
+# EMAIL_BACKEND = django.core.mail.backends.locmem.EmailBackend for production from guide
+SITE_NAME = "Luckerdogs"
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/django_static/'
